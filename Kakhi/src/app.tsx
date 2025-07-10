2ebad6a300dee424533c16fd9346b5864d4ef82a
𝕂𝕂 <127694228+4H-Darkmode@users.noreply.github.com>
2025-07-10 09:30:38 +0000

    the start

---
+++ Kakhi/src/app.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'preact/hooks'
+import './app.css'
+
+export function App() {
+
+
+  return (
+    <>
+      <body>
+        <div>
+          <img src="" alt="kakhi" />
+        </div>
+      </body>
+    </>
+  )
+}
